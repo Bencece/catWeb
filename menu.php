e282c8414cbed54c85798073f23c31b820bfc736
Zahorán Bence <h676093@stud.u-szeged.hu>
2019-05-21 13:30:16 +0200

    add menu.php and style

---
+++ menu.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Főoldal</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Információk</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Elérhetőség</a>
+    </li>
+  </ul>
+</nav>
